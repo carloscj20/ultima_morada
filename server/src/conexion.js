3db1473149ddfeeceafc7fea4979317d5644ff15
carloscj20 <136349561+carloscj20@users.noreply.github.com>
2023-06-28 10:50:46 -0400

    conexion to a base do datos by carlinhos

--- server/src/conexion.js
+++ server/src/conexion.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'root',
-    database: 'pokemon',
+    database: 'ultimamorada',
     port: 3306
 })
 
